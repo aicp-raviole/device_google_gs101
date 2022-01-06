1d07987130ba76d649d3d0dd20b1787e5adf9091
Scott Warner <Tortel1210@gmail.com>
2022-01-06 15:37:05 -0500

    Update sepolicy path
    Change-Id: I4bcee50514c02294137ea5ac6e64c64d69308bd4

--- sepolicy/gs101-sepolicy.mk
+++ sepolicy/gs101-sepolicy.mk
@@ -1,21 +1,21 @@
 # sepolicy that are shared among devices using whitechapel
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/whitechapel/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/whitechapel/vendor/google
 
 # unresolved SELinux error log with bug tracking
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/tracking_denials
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/private
 
 # Display
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/common
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/gs101
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/common
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/gs101
 
 # Micro sensor framework (usf)
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/usf
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/usf
 
 # system_ext
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/private
 
 #
 # Pixel-wide
@@ -36,4 +36,4 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/sscoredump
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_sniffer
 
 # Public
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/public
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/public
